7e2a6e1486228475a8d2f2f74d4febe629e7685a
BuiThuanIT02 <thuanbui2k215@gmail.com>
2023-10-18 15:27:38 +0700

    add style

--- WebApplication1/Controllers/MyTeam.cs
+++ WebApplication1/Controllers/MyTeam.cs
@@ -10,6 +10,7 @@ namespace WebApplication1.Controllers
     {
         public IActionResult Index()
         {
+            Console.Write("Helllo world");
             return View();
         }
     }
